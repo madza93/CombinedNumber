022d78046c25c0d559f36ea965e4d134fefea2d5
Maja Andrulewicz <61941student.wsb@gmail.com>
2019-02-11 18:02:53 +0100

    test failed

--- test/CombinedNumberTests.cpp
+++ test/CombinedNumberTests.cpp
@@ -10,24 +10,24 @@ struct CombinedNumberTests : public ::testing::Test
 
 TEST_F(CombinedNumberTests, whenEmptyListIsGivenEmptyStringShouldBeReturned)
 {
-    //std::list<unsigned int> l = {};
-
     auto result = cn.constructGreatestNumber({});
     ASSERT_EQ(result, "");
 }
 
 TEST_F(CombinedNumberTests, when1IsGivenString1IsReturned)
 {
-    //std::list<unsigned int> l = {};
-
     auto result = cn.constructGreatestNumber({1});
     ASSERT_EQ(result, "1");
 }
 
 TEST_F(CombinedNumberTests, when2IsGivenString2IsReturned)
 {
-    //std::list<unsigned int> l = {};
-
     auto result = cn.constructGreatestNumber({2});
     ASSERT_EQ(result, "2");
+}
+
+TEST_F(CombinedNumberTests, whenDoubleOneIsGivenString11IsReturned)
+{
+    auto result = cn.constructGreatestNumber({1,1});
+    ASSERT_EQ(result, "11");
 }
